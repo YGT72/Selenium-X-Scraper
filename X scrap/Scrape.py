741c6d1c881aec2d5e518dd0b35b56151bafc697
YGT72 <73789496+YGT72@users.noreply.github.com>
2024-07-10 23:13:51 +0100

    Update Scrape.py

--- X scrap/Scrape.py
+++ X scrap/Scrape.py
@@ -1,6 +1,10 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from pathlib import Path
 import os
 script_dir = Path(__file__).parent
@@ -13,5 +17,11 @@ service = Service(executable_path=str(ChromeDriver))
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://x.com/home")
+wait = WebDriverWait(driver, 10)
 
-input()
+signup = driver.find_element(By.XPATH, "//*[@id=\"react-root\"]/div/div/div[2]/main/div/div/div[1]/div/div/div[3]/div[3]/a")
+signup.click()
+
+input() 
+
+driver.quit()
